37cd39b28c2525c693e73ad4d1f41acddcb61f7e
PetricaH <petricahphp@gmail.com>
2024-10-03 13:40:39 +0300

    update

--- includes/banner.php
+++ includes/banner.php
@@ -1,3 +1,10 @@
+<?php if (isset($_SESSION['user']['username'])) {?>
+    <div class="logged_in_info">
+        <span>Welcome <?php echo $_SESSION['user']['username'] ?></span>
+        |
+        <span><a href="logout.php">logout</a></span>
+    </div>
+<?php } else { ?>
 <div class="banner">
     <div class="welcome_msg">
         <h1>Today's Inspiration</h1>
@@ -10,11 +17,15 @@
         <a href="register.php" class="btn">Join Us!</a>
     </div>
     <div class="login_div">
-        <form action="index.php" method="post">
+        <form action="<?php echo BASE_URL . 'index.php'; ?>" method="post">
             <h2>Login</h2>
+            <div style="width: 60%; margin= 0px auto;">
+                <?php include(ROOT_PATH . '/includes/errors.php'); ?>
+            </div>
             <input type="text" name="username" placeholder="Username"> 
             <input type="password" name="password" placeholder="Password">
             <button class="btn" type="submit" name="login_btn">Sign In</button>
         </form>
     </div>
 </div>
+<?php } ?>
