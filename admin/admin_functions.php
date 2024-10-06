b09052be7bea7d1d3bb340a48165769cc59764a2
PetricaH <petricahphp@gmail.com>
2024-10-06 10:25:55 +0300

    update

--- admin/admin_functions.php
+++ admin/admin_functions.php
@@ -82,7 +82,87 @@ function createAdmin($request_values){
     if (empty($password)) {
         array_push($errors, "Wrong Password");
     }
+    if ($password != $passwordConfirmation) { array_push($errors, "The two passwords do not matchs"); }
+    // ensure that no user is registered twice
+    // the email and username should be unique
+    $user_check_query = "SELECT * FROM users WHERE username='$username'
+                                                    OR email='$email' LIMIT 1";
+    $result = mysqli_query($conn, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+    if ($user) { // if user exists
+        if ($user['username'] === $username) {
+            array_push($errors, "username already exists");
+        }
+
+        if ($user['email'] === $email) {
+            array_push($errors, "Email already exists");
+        }
+    }
+    // register user if there are no errors in the form
+    if (count($errors) == 0) {
+        $password = md5($password); // encrypt the password before saving in the database
+        $query = "INSERT INTO users (username, email, role, password, created_at, updated_at)
+                            VALUES ('$username', '$email', '$role', '$password', now(), now())";
+        mysqli_query($conn, $query);
+
+        $_SESSION['message'] = "Admin user created successfully";
+        header('location: users.php');
+        exit(0);
+    }
 }
 
+// takes admin id as parameter
+// fetch the admin from database
+// sets admin fields on form for editing
+function editAdmin($admin_id) {
+    global $conn, $username, $role, $isEditingUser, $admin_id, $email;
+
+    $sql = "SELECT * FROM users WHERE id=$admin_id LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+    $admin = mysqli_fetch_assoc($result);
+
+    // set form values ($username and $email) on the form to be updated
+    $username = $admin['username'];
+    $email = $admin['email'];
+}
+
+// receives admin request from form and updates in the database
+function updateAdmin($request_values) {
+    global $conn, $errors, $role, $username, $isEditingUser, $admin_id, $email;
+    // get id of the admin to be updated
+    $admin_id = $request_values['admin_id'];
+    // set edit state to false
+    $isEditingUser = false;
+
+    $username = esc($request_values['username']);
+    $email = esc($request_values['email']);
+    $password = esc($request_values['password']);
+    $passwordConfirmation = esc($request_values['passwordConfirmation']);
+    if (isset($request_values['role'])) {
+        $role = $request_values['role'];
+    }
+    // register user if there are no errors  in the form
+    if (count($errors) == 0) {
+        // encrypt the password (security purposes)
+        $password = md5($password);
+
+        $query = "UPDATE users SET username='$username', email='$email', role='$role', password='$password' WHERE id=$admin_id";
+        mysqli_query($conn, $query);
+
+        $_SESSION['message'] = "Admin user updated successfully";
+        header('location: users.php');
+        exit(0);
+    }
+}
+// delete admin user
+function deleteAdmin($admin_id) {
+    global $conn;
+    $sql = "DELTE FROM users WHERE id=$admin_id";
+    if (mysqli_query($conn, $sql)) {
+        $_SESSION['message'] = "User successfully deleted";
+        header("location: users.php");
+        exit(0);
+    }
+}
 
 ?>
